1086982e7483ca35ff941128c27d1ad6a34469f0
Lori Buchanan <lori@jellyvision.com>
2019-11-09 12:13:14 -0600

    add more pictures

--- src/pictures.js
+++ src/pictures.js
@@ -10,37 +10,37 @@ export const pictures = [
     height: 1
   },
   {
+    src: require("./images/leaf.jpg"),
+    width: 1,
+    height: 1
+  },
+  {
+    src: require("./images/potion.jpg"),
+    width: 3,
+    height: 4
+  },
+  {
+    src: require("./images/toasty.jpg"),
+    width: 3,
+    height: 4
+  },
+  {
+    src: require("./images/fireplace.jpg"),
+    width: 4,
+    height: 3
+  },
+  {
+    src: require("./images/windowsill.jpg"),
+    width: 3,
+    height: 4
+  },
+  {
+    src: require("./images/light.jpg"),
+    width: 3,
+    height: 4
+  },
+  {
     src: require("./images/stargaze.jpg"),
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
     width: 4,
     height: 3
   }
