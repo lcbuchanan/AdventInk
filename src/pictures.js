fbce633e64c9f0076d241b38244024a1a253fa79
Lori Buchanan <lori@jellyvision.com>
2019-11-15 18:57:06 -0600

    add updated art

--- src/pictures.js
+++ src/pictures.js
@@ -1,46 +1,46 @@
 export const pictures = [
   {
+    src: require("./images/leaf2.jpg"),
+    width: 1,
+    height: 1
+  },
+  {
     src: require("./images/waterFall.jpg"),
     width: 3,
     height: 4
   },
   {
-    src: require("./images/mushrooms.jpg"),
-    width: 1,
-    height: 1
-  },
-  {
-    src: require("./images/leaf.jpg"),
-    width: 1,
-    height: 1
-  },
-  {
-    src: require("./images/potion.jpg"),
+    src: require("./images/potion2.jpg"),
     width: 3,
     height: 4
   },
   {
-    src: require("./images/toasty.jpg"),
+    src: require("./images/mushroom2.jpg"),
+    width: 1,
+    height: 1
+  },
+  {
+    src: require("./images/light2.jpg"),
     width: 3,
     height: 4
   },
   {
-    src: require("./images/fireplace.jpg"),
+    src: require("./images/hearth2.jpg"),
     width: 4,
     height: 3
   },
   {
-    src: require("./images/windowsill.jpg"),
+    src: require("./images/mallow2.jpg"),
+    width: 1,
+    height: 1
+  },
+  {
+    src: require("./images/window2.jpg"),
     width: 3,
     height: 4
   },
   {
-    src: require("./images/light.jpg"),
-    width: 3,
-    height: 4
-  },
-  {
-    src: require("./images/stargaze.jpg"),
+    src: require("./images/stargaze3.jpg"),
     width: 4,
     height: 3
   }
